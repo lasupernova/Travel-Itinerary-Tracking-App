82006d234d6067d0abc5fbf9df20b95f1e67a972
lasupernova <gabrilasuno@gmail.com>
2025-02-10 21:05:24 -0500

    added logger and loggin messages at initiation

--- travel_app.py
+++ travel_app.py
@@ -1,1 +1,13 @@
-print("Chris testing 123- but unsure about this change, please check")
+import os
+import logging
+
+# initiate logging
+logging.basicConfig(level=logging.DEBUG,
+format='%(asctime)s %(levelname)s %(message)s')
+#       filename='./tmp/myapp.log',
+    #   filemode='w'
+
+# log Notion token and DB ID used
+logging.info(f"Started travel app.")
+logging.info(f"Notion Token {os.environ['NOTION_TOKEN']}.")
+logging.info(f"Pulling info from Notion db with ID: {os.environ['DB_ID']}.")
