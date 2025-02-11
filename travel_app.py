80a90aa8e3900a29ca5adac4c5819574d211a0be
lasupernova <gabrilasuno@gmail.com>
2025-02-10 21:14:09 -0500

    added library imports

--- travel_app.py
+++ travel_app.py
@@ -1,5 +1,10 @@
 import os
 import logging
+from datetime import date, datetime
+import pandas as pd
+from notion_client import Client
+from notion_client.helpers import get_id
+from notion_client.helpers import collect_paginated_api
 
 # initiate logging
 logging.basicConfig(level=logging.DEBUG,
@@ -9,5 +14,5 @@ format='%(asctime)s %(levelname)s %(message)s')
 
 # log Notion token and DB ID used
 logging.info(f"Started travel app.")
-logging.info(f"Notion Token {os.environ['NOTION_TOKEN']}.")
-logging.info(f"Pulling info from Notion db with ID: {os.environ['DB_ID']}.")
+logging.info(f"Notion Token {os.environ['NOTION_TOKEN']}")
+logging.info(f"Pulling info from Notion db with ID: {os.environ['DB_ID']}")
