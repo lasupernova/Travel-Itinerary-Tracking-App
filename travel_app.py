d1b7929c85af5a5866f957e1f343a0d463b236f0
lasupernova <gabrilasuno@gmail.com>
2025-02-10 21:17:07 -0500

    defining variables (token, db ID + date to use for testing)

--- travel_app.py
+++ travel_app.py
@@ -12,7 +12,19 @@ format='%(asctime)s %(levelname)s %(message)s')
 #       filename='./tmp/myapp.log',
     #   filemode='w'
 
-# log Notion token and DB ID used
+
 logging.info(f"Started travel app.")
+
+# get personal Notion token from env variables; see Notion documentation to obtain token here: https://developers.notion.com/reference/create-a-token
+token = os.environ['NOTION_TOKEN']
 logging.info(f"Notion Token {os.environ['NOTION_TOKEN']}")
+
+# get id for main database containing the itinerary overview
+itinerary_db_id = os.environ['DB_ID']  
 logging.info(f"Pulling info from Notion db with ID: {os.environ['DB_ID']}")
+
+# define date to use for app (for testing: use date that is contained in Notion db, otherwise use today)
+# today = date.today()  #use future date that IS in Notion database for testing --> else error will be thrown as current_country() will return None
+date_string = "2026-02-12"
+format_string = "%Y-%m-%d"
+today = datetime.strptime(date_string, format_string)
