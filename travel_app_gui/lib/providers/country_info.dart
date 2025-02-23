ad894117d12a9a09451893ff31dd744d04e0eb0c
lasupernova <gabrilasuno@gmail.com>
2025-02-22 21:03:54 -0500

    created CountryInfo object to use as a provider for gui

---
+++ travel_app_gui/lib/providers/country_info.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CountryInfo {
+  // const PropertyItem({Key? key}) : super(key: key);
+  String id;
+  final String countryName;
+  final double currencyConversion;
+  final double temperature;
+  final double cloudCover;
+  final DateTime countryDate;
+
+  CountryInfo(
+      {this.id = "not given",
+      required this.countryName,
+      required this.currencyConversion,
+      required this.temperature,
+      required this.cloudCover,
+      required this.countryDate});
+}
